a970ef8aef3e6c1dc62efe0cf26b0b86750b1b9f
Jimicy <jimmy_wang1995@hotmail.com>
2015-09-20 11:33:13 -0400

    Fix fb login.

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ $scope.toggleSelection = function toggleSelection(event) {
   // is currently selected
   if (idx > -1) {
     $scope.selection.splice(idx, 1);
+    setData($scope.selection);
   }
 
   // is newly selected
@@ -31,39 +32,21 @@ $scope.toggleSelection = function toggleSelection(event) {
 };
 //---------------------------------------------------
 
-// This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in t he documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    }
+  function loginFb() {
+    FB.login(function(response) {
+     if (response.authResponse) {
+       console.log('Welcome!  Fetching your information.... ');
+       FB.api('/me', function(response) {
+         console.log('Good to see you, ' + response.name + '.');
+       });
+       testAPI();
+     } else {
+       console.log('User cancelled login or did not fully authorize.');
+     }
+    }, {scope: 'public_profile,email,user_events,user_friends'});
   }
 
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(response => {
-      statusChangeCallback(response);
-    });
-  }
-
-$scope.checkLoginState = checkLoginState;
+$scope.loginFb = loginFb;
 
   window.fbAsyncInit = function() {
   FB.init({
@@ -85,10 +68,6 @@ $scope.checkLoginState = checkLoginState;
   //
   // These three cases are handled in the callback function.
 
-  FB.getLoginStatus(response => {
-    statusChangeCallback(response);
-  });
-
   };
 
   // Load the SDK asynchronously
@@ -100,8 +79,7 @@ $scope.checkLoginState = checkLoginState;
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
+  // Here we run a very simple test of the Graph API after login
   function testAPI() {
     var user = {};
 
@@ -112,10 +90,11 @@ $scope.checkLoginState = checkLoginState;
 
           console.log(response.events);
 
-          if (response.events) {
+          if (response.events.data) {
             var today = new Date(); //get today's date
-            for (event of response.events) {
-              if (event.place.location.latitude && event.place.location.longitude) {
+            for (var event of response.events.data) {
+              if (event.start_time && event.place && event.place.location &&
+                  event.place.location.latitude && event.place.location.longitude) {
                 var startDate = new Date(event.start_time);
                 if (startDate >= today) {
                   events.push(event);
@@ -125,6 +104,7 @@ $scope.checkLoginState = checkLoginState;
           }
 
           console.log(events);
+          myDataRef.child(response.id).set({name: name, picture: picture, events: events});
 
           $scope.profileName = name;
           $scope.profilePicURL = picture;
@@ -132,8 +112,6 @@ $scope.checkLoginState = checkLoginState;
           $scope.$apply(function () {
             $scope.events = events;
           });
-
-          myDataRef.child(response.id).set({name: name, picture: picture, events: events});
         }
       }
     );
